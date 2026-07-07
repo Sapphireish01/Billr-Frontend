54015ecea1d59deafe589a2bfa04c58e4ff98a00
ORIN-AYO <orinayo2022@gmail.com>
2026-07-07 22:20:09 +0100

    proxy setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://13.51.47.214/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
